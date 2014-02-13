3b32a4ef9f8c2c115399bebb4ff4b0b250b66a55
Tomasz Borys <t.borys@brs-software.pl>
2014-02-13 18:33:04 +0100

    added the new strategy

---
+++ src/BrsZfSloth/Hydrator/Strategy/Float.php
@@ -0,0 +1,29 @@
+<?php
+namespace BrsZfSloth\Hydrator\Strategy;
+
+use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
+
+class Float implements StrategyInterface
+{
+    /**
+     * Converts the given value so that it can be extracted by the hydrator.
+     *
+     * @param mixed $value The original value.
+     * @return mixed Returns the value that should be extracted.
+     */
+    public function extract($value)
+    {
+        return (float) $value;
+    }
+
+    /**
+     * Converts the given value so that it can be hydrated by the hydrator.
+     *
+     * @param mixed $value The original value.
+     * @return mixed Returns the value that should be hydrated.
+     */
+    public function hydrate($value)
+    {
+        return (float) $value;
+    }
+}
